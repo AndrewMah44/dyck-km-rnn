372cde5404d61c68116ed54be7e78565ffd9de5f
AndrewMah44 <andrew.mah44@gmail.com>
2026-05-26 15:24:05 -0400

    Update losses.py

--- src/dyck_rnn/training/losses.py
+++ src/dyck_rnn/training/losses.py
@@ -1,4 +1,5 @@
 import jax
+import jax.numpy as jnp
 from jax.scipy.special import kl_div
 from optax import softmax_cross_entropy_with_integer_labels
 
@@ -6,7 +7,7 @@ def pred_loss_func(model, x, y, mask):
     logits = model(x)
     loss = softmax_cross_entropy_with_integer_labels(logits, y) * mask
 
-    return sum(loss) / sum(mask)
+    return jnp.sum(loss) / jnp.sum(mask)
 
 def masked_kl_div(opt_dist, mdl_dist, mask):
     mdl_dist = mdl_dist + 1e-32
