9549c67231c50e149722ed9533480287c4f3deab
AndrewMah44 <andrew.mah44@gmail.com>
2026-05-26 10:33:10 -0400

    add KL divergence

--- src/dyck_rnn/training/losses.py
+++ src/dyck_rnn/training/losses.py
@@ -1,4 +1,5 @@
 import jax
+from jax.scipy.special import kl_div
 from optax import softmax_cross_entropy_with_integer_labels
 
 def pred_loss_func(model, x, y, mask):
@@ -6,3 +7,7 @@ def pred_loss_func(model, x, y, mask):
     loss = softmax_cross_entropy_with_integer_labels(logits, y) * mask
 
     return sum(loss) / sum(mask)
+
+def masked_kl_div(opt_dist, mdl_dist, mask):
+    mdl_dist = mdl_dist + 1e-32
+    return jnp.sum(kl_div(opt_dist, mdl_dist).sum(-1) * mask) / jnp.sum(mask)
