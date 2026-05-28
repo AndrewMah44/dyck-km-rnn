0fe6f851ca3c6689f58740ef156614d67d2b2aad
AndrewMah44 <andrew.mah44@gmail.com>
2026-05-28 10:46:04 -0400

    Move to utils

--- src/dyck_rnn/utils/get_depth.py
+++ src/dyck_rnn/utils/get_depth.py
@@ -2,5 +2,5 @@ import jax.numpy as jnp
 
 def get_depth(state, k, m):
     depth = jnp.floor(jnp.log(state*(k - 1) + 1) / jnp.log(k))
-    
-    return jnp.where(depth < m, depth, -1)
+    return depth
+    # return jnp.where(depth <= m, depth, -1)
