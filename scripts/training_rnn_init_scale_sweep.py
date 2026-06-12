0ed5ef9c58d3cfea225c57a39914f19e3db9dfb9
AndrewMah44 <andrew.mah44@gmail.com>
2026-06-12 13:40:08 -0400

    Changing name to be more specific

---
+++ scripts/training_rnn_init_scale_sweep.py
@@ -0,0 +1,68 @@
+# Imports
+import jax
+jax.config.update("jax_enable_x64", True)
+
+import yaml
+import argparse
+import jax.numpy as jnp
+from pathlib import Path
+from copy import deepcopy
+from dyck_rnn.training.train import train_dyck_rnn
+
+# ====== Load Configuration File ======
+parser = argparse.ArgumentParser()
+parser.add_argument("--config", type=Path, required=True)
+args = parser.parse_args()
+
+with args.config.open("r") as f:
+    sweep_config = yaml.safe_load(f)
+
+# ====== Set Up Run Name ======
+k = sweep_config["data"]["k"]
+m = sweep_config["data"]["m"]
+task = sweep_config["experiment"]["task"]
+n_runs = sweep_config["experiment"]["n_runs"]
+seed = sweep_config["experiment"]["seed"]
+
+# Linear and LSTM hidden sizes are based on Hewitt. 
+# GRU is to match number of linear params
+if sweep_config['model']['cell_type'].lower() == 'linear':
+    sweep_config['model']['hidden_size'] = int(jnp.ceil(6 * m * jnp.log2(k)))
+
+elif sweep_config['model']['cell_type'].lower() == 'lstm':
+    sweep_config['model']['hidden_size'] = int(jnp.ceil(3 * m * jnp.log2(k)))
+
+elif sweep_config['model']['cell_type'].lower() == 'gru':
+    sweep_config['model']['hidden_size'] = int(jnp.floor(
+        0.6 * jnp.ceil(6 * m * jnp.log2(k))))
+
+sweep_name = task \
+    + f'_k{sweep_config["data"]["k"]}' \
+    + f'_m{sweep_config["data"]["m"]}' \
+    + f'_{sweep_config["model"]["cell_type"]}' \
+    + f'_h{sweep_config['model']['hidden_size']}' \
+    + f'_mlp{sweep_config["model"]["readout_depth"]}' \
+    + '_InitScaleSweep'
+
+print(f"Fitting {sweep_name}...")
+
+# ====== Set Up Paths ======
+full_run_dir = Path("runs") / sweep_name
+full_run_dir.mkdir(parents=True, exist_ok=True)
+
+with (full_run_dir / "sweep_config.yaml").open("w") as f:
+    yaml.safe_dump(sweep_config, f)
+
+# ====== Sweep over initial scale ======
+for init_scale in sweep_config['sweep']['init_scale']:
+    print(init_scale)
+
+    run_config = deepcopy(sweep_config)
+    run_config['model']['init_scale'] = init_scale
+
+    run_name = f"fit_{init_scale}"
+    train_dyck_rnn(run_name, run_config, run_parent=full_run_dir)
+
+
+
+
