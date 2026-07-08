455a4f5ff21930f32c31bb6dd6b0ec8f8d17db74
AndrewMah44 <andrew.mah44@gmail.com>
2026-07-08 14:32:20 -0400

    Update evaluate_model_performance.py

--- scripts/evaluate_model_performance.py
+++ scripts/evaluate_model_performance.py
@@ -8,15 +8,13 @@ import matplotlib as mpl
 mpl.rcParams['pdf.fonttype'] = 42
 mpl.rcParams['ps.fonttype'] = 42
 
+import json
 import yaml
-import argparse
-import pickle as pkl
 import jax.numpy as jnp
 import jax.random as jr
 from pathlib import Path
-from itertools import product
 import matplotlib.pyplot as plt
-from jax.scipy.special import kl_div
+from jax.scipy.special import rel_entr
 from dyck_rnn.data.samplers import powerlaw
 
 from dyck_rnn.data.dyck_hmm import dyck_hmm
@@ -91,20 +89,20 @@ def get_close_conditional_prob(model_probs, seqs, k):
     
     return confidence, confidence_mat
 #%%
+
+
 run_name = "DyckKM_k02_m04_linear_h24_mlp1"
-run = 0
+# run_name = "DyckKM_k02_m04_linear_h24_mlp1_adamw"
+# run_name = "DyckKM_k02_m04_linear_h24_mlp0"
+# run_name = "DyckKM_k02_m04_gru_h24_mlp0"
+# run_name = "DyckKM_k02_m04_linear_h24_mlp2"
+# run_name = "DyckKM_k02_m04_linear_h64_mlp1"
 
-model_dir = "/Users/amah/Documents/GitHub/dyck-km-rnn/runs/old2"
-run_dir = Path("/Users/amah/Documents/GitHub/dyck-km-rnn/runs/") \
-    / run_name / f"run_{run:02}"
-
-# ==== Load Linear Model ====
-with open(run_dir / "config.yaml", "r") as file:
+# ==== Load odel Configs ====
+model_dir = "/Users/amah/Documents/GitHub/dyck-km-rnn/runs/"
+run_dir = Path("/Users/amah/Documents/GitHub/dyck-km-rnn/runs/")
+with open(run_dir /run_name / "run_00/config.yaml", "r") as file:
     model_config = yaml.safe_load(file)
-
-model = load_model(
-    run_name + f"/run_{run:02}", 
-    run_parent_dir = model_dir)
 
 # ==== Generate Test Dataset ====
 master_key = jr.PRNGKey(eval_config['experiment']['seed'])
@@ -112,14 +110,14 @@ data_key, length_key, sample_key = jr.split(master_key,3)
 
 k = model_config['data']['k']
 m = model_config['data']['m']
-max_length = 50*(4 * m * (m + 4))
+max_length = 75*(4 * m * (m + 4)) 
 
-DyckHMM = dyck_hmm(k, m)
+DyckHMM = dyck_hmm(k, m, build_full_model=True)
 
 lengths = powerlaw(
     length_key, 
     15, 
-    max_length, 
+    100*(4 * m * (m + 4)), 
     model_config['data']['alpha'], 
     shape=(eval_config['data']['test_size'],)
 )
@@ -129,87 +127,241 @@ states, sequences = DyckHMM.batch_sample_sequence(
     num_timesteps = max_length, 
     min_length = lengths, 
     key = data_key)
+states = states[:,:max_length]
+sequences = sequences[:,:max_length]
 
-# ==== Run model of data
-model_probs = jax.nn.softmax(jax.vmap(model)(sequences))
+mask = sequences != (2 * k + 1)
+opt_probs = DyckHMM.batch_one_step_prediction(sequences).transpose(0, 2, 1)
 
-# ==== Confidence Metrics ====
-conditional_prob, conditional_prob_mat = get_close_conditional_prob(
-    model_probs, sequences, k)
+models = []
+mean_conditional_prob_mat = jnp.zeros((10,1000,64))
+std_conditional_prob_mat = jnp.zeros((10,1000,64))
 
-num_states = int((k ** (m+1) - 1) / (k - 1))
-ages = jax.vmap(close_ages, in_axes=[0,0,None,None])(
-    sequences, states, num_states, k)
-ages = ages[(sequences >= k) & (sequences < 2*k)]
+final_validation_loss = jnp.zeros(10)
+for run in range(10):
+    model = load_model(
+        run_name + f"/run_{run:02}", 
+        run_parent_dir = model_dir)
+    models.append(model)
 
-# ==== Plots ====
-fig, ax = plt.subplots(3, 1)
+    # ==== Run model of data ====
+    model_probs = jax.nn.softmax(jax.vmap(model)(sequences))
 
-# Confidence histogram
-ax[0].hist(conditional_prob)
+    # =======================================
+    # Figure 1 - Checking Probability outputs
+    # =======================================
+    d_kl_mat = jnp.where(mask, rel_entr(opt_probs, model_probs).sum(2), jnp.nan)
+    d_kl_mat = jnp.where(states!=0, d_kl_mat, jnp.nan)
 
-ax[0].set_xlabel('Conditional Prob.')
-ax[0].set_ylabel('N (observations)')
+    fig, axes = plt.subplots(2, k+1, figsize=(10, 6))
 
-ax[0].set_yscale('log')
-ax[0].set_title(f"Mean: {jnp.mean(conditional_prob)}")
+    cond = states == 0
+    x = jnp.arange(2*k + 1)
+    y = jnp.nanmean(model_probs[cond], axis=0)[:-1]
+    yerr = jnp.nanstd(model_probs[cond], axis=0)[:-1]
+    y_opt = opt_probs[cond].mean(0)
 
-# Confidence as a function of age
-n_bins = 25
-bins = jnp.linspace(0, jnp.max(ages), n_bins)
-idx = jnp.digitize(ages, bins)
+    for i in range(k):
+        # ==== Solid Blue States ====
+        cond1 = (states % 2 == i) & (states < k ** m - 1) & (states != 0)
+        x = jnp.arange(2*k + 1)
+        y = jnp.nanmean(model_probs[cond1], axis=0)[:-1]
+        yerr = jnp.nanstd(model_probs[cond1], axis=0)[:-1]
 
-y = jnp.array(
-    [jnp.nanmean(conditional_prob[idx==i]) for i in range(n_bins)])
-yerr = jnp.array(
-    [jnp.nanstd(conditional_prob[idx==i]) for i in range(n_bins)])
-counts = jnp.array(
-    [jnp.sum(idx==i) for i in range(n_bins)])
+        y_opt = opt_probs[cond1].mean(0)
 
-ax[1].errorbar(bins, y, yerr/jnp.sqrt(counts), marker='o', linestyle='None')
-ax[1].set_xlabel('Bracket age')
-ax[1].set_ylabel('Conditional Prob.')
+        axes[0,i].plot(x, y_opt[:-1], color='k')
+        axes[0,i].plot(x, y)
+        axes[0,i].fill_between(x, y+yerr, y-yerr, alpha=0.25)
+        axes[0,i].set_title(f'{i}, Non-terminal')
 
-N_bins = 2**6
-binned = conditional_prob_mat.reshape(
-    conditional_prob_mat.shape[0], N_bins, -1)
-trial_bin_means = jnp.nanmean(binned, axis=2)
-counts = jnp.sum(~jnp.isnan(binned), axis=[0,2])
+        # ==== Solid Red States ====
+        cond2 = (states % 2 == i) & (states >= k ** m - 1) & (states < k ** (m+1) - 1)
+        x = jnp.arange(6)[:-1]
+        y = jnp.nanmean(model_probs[cond2], axis=0)[:-1]
+        yerr = jnp.nanstd(model_probs[cond2], axis=0)[:-1]
+
+        y_opt = opt_probs[cond2].mean(0)
+
+        axes[1,i].plot(x, y_opt[:-1], color='k')
+        axes[1,i].plot(x, y)
+        axes[1,i].fill_between(x, y+yerr, y-yerr, alpha=0.25)
+        axes[1,i].set_title(f'{i}, Terminal')
+
+    # ==== D_KL Histogram ====
+    axes[0,k].hist(d_kl_mat.flatten())
+    axes[0,k].set_yscale('log')
+
+    axes[0,k].set_xlabel('D_KL')
+    axes[0,k].set_ylabel('Count')
+
+    # ==== D_KL By Sequence Position ====
+    axes[1,k].plot(jnp.arange(1, max_length+1), jnp.nanmedian(d_kl_mat, axis=0))
+
+    axes[1,k].axvline(4 * m * (m + 4), color='k', linestyle='--')
+
+    axes[1,k].set_yscale('log')
+    axes[1,k].set_xscale('log')
+
+    axes[1,k].set_xlabel('Token Position')
+    axes[1,k].set_ylabel('Median D_KL')
+
+    fig.suptitle(f'{run}\nRaw Model Outputs')
+    fig.tight_layout()
+
+    # ==================================
+    # Figure 2 - Close coditional Probs.
+    # ==================================
+    conditional_prob, conditional_prob_mat = get_close_conditional_prob(
+        model_probs, sequences, k)
+
+    num_states = int((k ** (m+1) - 1) / (k - 1))
+    ages = jax.vmap(close_ages, in_axes=[0,0,None,None])(
+        sequences, states, num_states, k)
+    ages = ages[(sequences >= k) & (sequences < 2*k)]
+
+    # ==== Plots ====
+    fig, ax = plt.subplots(3, 1)
+
+    # Confidence histogram
+    ax[0].hist(conditional_prob)
+
+    ax[0].set_xlabel('Conditional Prob.')
+    ax[0].set_ylabel('N (observations)')
+
+    ax[0].set_yscale('log')
+    ax[0].set_title(f"Mean: {jnp.mean(conditional_prob)}")
+
+    # Confidence as a function of age
+    n_bins = 50
+    bins = jnp.linspace(0, jnp.max(ages), n_bins)
+    idx = jnp.digitize(ages, bins)
+
+    y = jnp.array(
+        [jnp.nanmean(conditional_prob[idx==i]) for i in range(n_bins)])
+    yerr = jnp.array(
+        [jnp.nanstd(conditional_prob[idx==i]) for i in range(n_bins)])
+    counts = jnp.array(
+        [jnp.sum(idx==i) for i in range(n_bins)])
+
+    ax[1].errorbar(bins, y, yerr/jnp.sqrt(counts), marker='o', linestyle='None')
+    ax[1].set_xlabel('Bracket age')
+    ax[1].set_ylabel('Conditional Prob.')
+
+    N_bins = 2**6
+    binned = conditional_prob_mat.reshape(
+        conditional_prob_mat.shape[0], N_bins, -1)
+    trial_bin_means = jnp.nanmean(binned, axis=2)
+    counts = jnp.sum(~jnp.isnan(binned), axis=[0,2])
+
+    mean_conditional_prob_mat = mean_conditional_prob_mat.at[run].set(
+        trial_bin_means
+    )
+
+    x = jnp.linspace(0, max_length, N_bins)
+    y = jnp.nanmean(trial_bin_means, axis=0)
+    yerr = jnp.nanstd(trial_bin_means, axis=0)
+
+    ax[2].plot(x, y)
+    ax[2].fill_between(x, y+yerr, y-yerr, alpha=0.25)
+    ax[2].axvline(4 * m * (m + 4))
+
+    ax[2].set_xlabel('Sequence position')
+    ax[2].set_ylabel('Conditional Prob.')
+
+    fig.suptitle(f'{run}\nClose Token Conditional Prob.')
+    fig.tight_layout()
+
+    with open(run_dir / run_name / f"run_{run:02}/metrics.json", "r") as file:
+        a = json.load(file)
+
+    print(f"{run}: {a['validation_loss_history'][-1]}")
+    final_validation_loss = final_validation_loss.at[run].set(
+        a['validation_loss_history'][-1]
+    )
+
+
+# %% Average Close Conditional Prob over Seeds - by seq position
 
 x = jnp.linspace(0, max_length, N_bins)
-y = jnp.nanmean(trial_bin_means, axis=0)
-yerr = jnp.nanstd(trial_bin_means, axis=0) / jnp.sqrt(counts)
 
-ax[2].plot(x, y)
-ax[2].fill_between(x, y+yerr, y-yerr, alpha=0.25)
-ax[2].axvline(4 * m * (m + 4))
+for i in range(10):
+    y = jnp.nanmean(mean_conditional_prob_mat[i], axis=0)
+    yerr = jnp.nanstd(mean_conditional_prob_mat[i], axis=0)
 
-ax[2].set_xlabel('Sequence position')
-ax[2].set_ylabel('Conditional Prob.')
+    plt.plot(x, y)
+    plt.fill_between(x, y+yerr, y-yerr, alpha=0.25)
+
+plt.ylabel('True Close Conditional Prob.')
+plt.xlabel('')
+
+# %% Median D_KL over Seeds - by seq position
+
+d_kl_mats = jnp.zeros((10, 1000, 9600))
+for run in range(10):
+    model = models[run]
+    
+    # ==== Run model of data ====
+    model_probs = jax.nn.softmax(jax.vmap(model)(sequences))
+
+    d_kl_mat = jnp.where(mask, rel_entr(opt_probs, model_probs).sum(2), jnp.nan)
+    d_kl_mat = jnp.where(states!=0, d_kl_mat, jnp.nan)
+
+    d_kl_mats = d_kl_mats.at[run].set(d_kl_mat)
+
+d_kl_mats_binned = d_kl_mats.reshape([10, 1000, -1, (4 * m * (m + 4))])
+
+x = jnp.linspace(1, d_kl_mats.shape[-1], 75)
+y = jnp.nanmedian(d_kl_mats_binned, axis=(1,3))
+q25, q75 = jnp.nanpercentile(
+    d_kl_mats_binned,
+    jnp.array([25, 75]),
+    axis=(1, 3),
+)
+
+for i in range(10):
+    plt.plot(x, y[i], label=f'Model {i}')
+    plt.fill_between(x, q25[i], q75[i], alpha=0.25)
+
+plt.xlabel('Token Position')
+plt.ylabel('Median D_KL')
+
+plt.title(f'Dyck-({k},{m})\nLinear + MLP')
+plt.axvline(4 * m * (m+4))
+plt.xscale("log")
+plt.yscale("log")
 
 
-#%%
-fig, axes = plt.subplots(3,3, figsize=(10,6))
+# %% Binned Median D_KL over Seeds - in- vs. out-of-distribution
 
-model_performance("DyckKM_k02_m04_linear_h24_mlp1", axes[:,0], run = 0)
-# model_performance("DyckKM_k02_m04_lstm_h12_mlp0", axes[:,2], run = 0)
+colors=['k', 'b', 'r', 'g', 'm'] * 2
+for idx in range(9):
+    data1 = d_kl_mats[idx,:,:4 * m * (m + 4)]
+    q25, q75 = jnp.nanpercentile(data1, jnp.array([25, 75]))
+    plt.errorbar(idx-0.2, 
+                jnp.nanmedian(data1), 
+                jnp.array([q25, q75])[:,None],
+                marker='o',
+                color=colors[idx],
+                label='Early')
 
-yl0 = axes[0,1].get_ylim()[1]
-yl1 = axes[1,0].get_ylim()[0]
-yl2 = axes[2,0].get_ylim()[0]
+    data2 = d_kl_mats[idx,:,4 * m * (m + 4):]
+    q25, q75 = jnp.nanpercentile(data2, jnp.array([25, 75]))
+    plt.errorbar(idx+0.2, 
+                jnp.nanmedian(data2), 
+                jnp.array([q25, q75])[:,None],
+                marker='v',
+                color=colors[idx],
+                label='Late')
+      
+    plt.plot([idx-0.2, idx+0.2], 
+             [jnp.nanmedian(data1),jnp.nanmedian(data2)],
+             color=colors[idx])
 
-[ax.set_ylim([0, yl0]) for ax in axes[0,:]]
-[ax.set_ylim([yl1, 1.025]) for ax in axes[1,:]]
-[ax.set_ylim([yl2, 1.01]) for ax in axes[2,:]]
-# [ax.set_xlim([0, 1]) for ax in axes[0,:]]
+plt.xticks(range(9))
+plt.yscale('log')
 
-fig.tight_layout()
-# fig.tight_layout()
-# fig.savefig('/Users/amah/Desktop/fig1.pdf')
+plt.ylabel('Median D_KL')
+plt.xlabel('Model')
+plt.title('Linear + MLP Model')
 
-#%%
-
-fig, axes = plt.subplots(3,1, figsize=(3,6))
-
-model_performance("DyckKM_k04_m04_linear_h64_mlp1", axes, run = 0)
-# %%
