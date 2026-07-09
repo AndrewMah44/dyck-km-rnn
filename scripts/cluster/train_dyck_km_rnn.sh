c0286aae75dff0f6055fa97c4ee10cf1b7d9bf47
Andrew Mah <andrew.mah44@gmail.com>
2026-07-09 15:46:32 -0400

    editing job

--- scripts/cluster/train_dyck_km_rnn.sh
+++ scripts/cluster/train_dyck_km_rnn.sh
@@ -9,23 +9,12 @@
 #SBATCH --mail-type=END,FAIL,ARRAY_TASKS
 #SBATCH --mail-user=amah@flatironinstitute.org
 #SBATCH --gpus=1
+#SBATCH -p gpu
 
 module --force purge
 # module load python   # comment this out unless you know the correct module name
 source ~/venvs/dyck_rnn/bin/activate
 
-echo "hostname: $(hostname)"
-echo "SLURM_JOB_ID=$SLURM_JOB_ID"
-echo "SLURM_JOB_PARTITION=$SLURM_JOB_PARTITION"
-echo "SLURM_GPUS=$SLURM_GPUS"
-echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
-
-which python
-python --version
-nvidia-smi
-
-python -c "import jax; print('jax', jax.__version__); print(jax.devices())"
-
 python3 scripts/training_dyck_km_models.py \
   --config experiments/config_train_dyck_km_rnn.yaml \
-  --$SLURM_ARRAY_TASK_ID
+  --r $SLURM_ARRAY_TASK_ID
