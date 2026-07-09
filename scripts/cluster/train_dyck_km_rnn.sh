a7e2c38a685807813b32c2b59763e043a2d38665
AndrewMah44 <andrew.mah44@gmail.com>
2026-07-09 15:48:37 -0400

    Update train_dyck_km_rnn.sh

--- scripts/cluster/train_dyck_km_rnn.sh
+++ scripts/cluster/train_dyck_km_rnn.sh
@@ -3,17 +3,17 @@
 #SBATCH --output=logs/dyck_%A_%a.out
 #SBATCH --error=logs/dyck_%A_%a.err
 #SBATCH --array=1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=1
 #SBATCH --mem=15G
 #SBATCH --time=5:00:00
 #SBATCH --mail-type=END,FAIL,ARRAY_TASKS
 #SBATCH --mail-user=amah@flatironinstitute.org
-#SBATCH --gpus=1
-#SBATCH -p gpu
+#SBATCH --partition=genx
 
 module --force purge
 # module load python   # comment this out unless you know the correct module name
 source ~/venvs/dyck_rnn/bin/activate
+export JAX_PLATFORMS=cpu
 
 python3 scripts/training_dyck_km_models.py \
   --config experiments/config_train_dyck_km_rnn.yaml \
